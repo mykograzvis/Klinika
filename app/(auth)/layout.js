bfdb001e74fee0bb4f0bb656e5dfa8dede6df8d1
mykograzvis <mykograzvis@gmail.com>
2026-02-12 20:02:20 +0200

    statine information page
    viskas suloadinta is kart tai nelagina nieko, paveikslelius loudina isnta

--- app/(auth)/layout.js
+++
@@ -1,8 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div className="auth-container">
-      {/* Čia nėra jokio Navbar, tik grynas puslapis */}
-      {children}
-    </div>
-  );
-}
