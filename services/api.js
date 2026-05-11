bd389aa1aa858bed01d939b858eeb9887731ff1a
mykograzvis <mykograzvis@gmail.com>
2026-05-11 13:08:48 +0300

    api keiciam

--- services/api.js
+++ services/api.js
@@ -1,3 +1,3 @@
-//const API_URL = "https://odonto-api-fqdae8f2hqcrdgch.swedencentral-01.azurewebsites.net";
-const API_URL = "https://localhost:7237";
+const API_URL = "https://odonto-api-fqdae8f2hqcrdgch.swedencentral-01.azurewebsites.net";
+//const API_URL = "https://localhost:7237";
 export default API_URL;
